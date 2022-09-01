bf81cf2b0d9a06c4f7f8e612d598d9b6a97b4511
ridwan <ridwanediallo99@gmail.com>
2022-09-01 15:40:26 +0000

    Add decribe reverse

--- spec/solver.spec.rb
+++ spec/solver.spec.rb
@@ -16,4 +16,18 @@ describe Solver do
       expect(solve.factorial(10)).to eq 3_628_800
     end
   end
+
+  describe "#reverse" do
+    it "should reverse the given string" do
+      expect(solve.reverse("hello")).to be("olleh")
+    end
+
+    it "should reverse the given string" do
+      expect(solve.reverse("rich")).to be("hcir")
+    end
+
+    it "should reverse the given string" do
+      expect(solve.reverse("ridwan")).to be("nawdir")
+    end
+  end
 end
