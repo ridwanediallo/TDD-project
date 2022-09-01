bd617edebd34042e328ff103cc93267f4ecccfb3
ridwan <ridwanediallo99@gmail.com>
2022-09-01 15:39:26 +0000

    Update describ factorial

--- spec/solver.spec.rb
+++ spec/solver.spec.rb
@@ -5,22 +5,15 @@ describe Solver do
 
   describe "#factorial" do
     it "should return the factorial of the given number" do
-      solve.factorial(5)
-
       expect(solve.factorial(5)).to eq 120
     end
 
     it "should return the factorial of the given number" do
-      solve.factorial(1)
-
       expect(solve.factorial(1)).to eq 1
     end
 
     it "should return the factorial of the given number" do
-      solve.factorial(10)
-
       expect(solve.factorial(10)).to eq 3_628_800
     end
   end
-
 end
