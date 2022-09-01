06e9a7434c6e79fb31381ef974a15f9463535fee
ridwan <ridwanediallo99@gmail.com>
2022-09-01 15:21:34 +0000

    Add Factorial rspec

--- spec/solver.spec.rb
+++ spec/solver.spec.rb
@@ -1,1 +1,25 @@
 require_relative '../solver'
+
+RSpec.describe Solver do
+let(:solve) {Solver.new }
+   it "should return the factorial of the given number" do
+    solve.factorial(5)
+
+    expect(solve.factorial(5)).to eq 120
+   end
+
+   it "should return the factorial of the given number" do
+    solve.factorial(1)
+
+    expect(solve.factorial(1)).to eq 1
+   end
+
+   it "should return the factorial of the given number" do
+    solve.factorial(10)
+
+    expect(solve.factorial(10)).to eq 3628800
+   end
+
+
+end
+
