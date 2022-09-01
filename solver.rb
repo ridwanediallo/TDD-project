56b5c6d89d6cb5a2eaddda6375cd7629bbc25e84
ridwan <ridwanediallo99@gmail.com>
2022-09-01 18:36:39 +0000

    fix linters

--- solver.rb
+++ solver.rb
@@ -1,28 +1,27 @@
 class Solver
+  def reverse(string)
+    string.reverse
+  end
 
-    def reverse(string)
-        string.reverse
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
     end
- 
-    def fizzbuzz(number)
-        if number % 3 == 0 && number % 5 == 0
-            "fizzbuzz"
-        elsif number % 3 == 0
-            "fizz"
-        elsif number % 5 == 0
-            "buzz"
-        else
-            number.to_s
-        end
-    end
+  end
 
-    def factorial(number)
-        if number == 0 || number == 1
-            1
-        elsif number < 0
-         raise  "Please enter a positive Integer"
-        else
-            number * factorial(number - 1)
-        end
+  def factorial(number)
+    if [0, 1].include?(number)
+      1
+    elsif number.negative?
+      raise 'Please enter a positive Integer'
+    else
+      number * factorial(number - 1)
     end
+  end
 end
--- spec/solver.spec.rb
+++ spec/solver.spec.rb
@@ -1,51 +1,43 @@
-require_relative "../solver"
+require_relative '../solver'
 
 describe Solver do
   let(:solve) { Solver.new }
-
-  describe "#factorial" do
-    it "should return the factorial of the given number" do
+  describe '#factorial' do
+    it 'should return the factorial of the given number' do
       expect(solve.factorial(5)).to eq 120
     end
-
-    it "should return the factorial of the given number" do
+    it 'should return the factorial of the given number' do
       expect(solve.factorial(1)).to eq 1
     end
-
-    it "should return the factorial of the given number" do
+    it 'should return the factorial of the given number' do
       expect(solve.factorial(10)).to eq 3_628_800
     end
   end
 
-  describe "#reverse" do
-    it "should reverse the given string" do
-      expect(solve.reverse("hello")).to eq("olleh")
+  describe '#reverse' do
+    it 'should reverse the given string' do
+      expect(solve.reverse('hello')).to eq('olleh')
     end
-
-    it "should reverse the given string" do
-      expect(solve.reverse("rich")).to eq("hcir")
+    it 'should reverse the given string' do
+      expect(solve.reverse('rich')).to eq('hcir')
     end
-
-    it "should reverse the given string" do
-      expect(solve.reverse("ridwan")).to eq("nawdir")
+    it 'should reverse the given string' do
+      expect(solve.reverse('ridwan')).to eq('nawdir')
     end
   end
 
-  describe "#fizzbuzz" do
-    it "should return the correct string" do
-      expect(solve.fizzbuzz(3)).to eq("fizz")
+  describe '#fizzbuzz' do
+    it 'should return the correct string' do
+      expect(solve.fizzbuzz(3)).to eq('fizz')
     end
-
-    it "should return the correct string" do
-      expect(solve.fizzbuzz(5)).to eq("buzz")
+    it 'should return the correct string' do
+      expect(solve.fizzbuzz(5)).to eq('buzz')
     end
-
-    it "should return the correct string" do
-      expect(solve.fizzbuzz(15)).to eq("fizzbuzz")
+    it 'should return the correct string' do
+      expect(solve.fizzbuzz(15)).to eq('fizzbuzz')
     end
-
-    it "should return the correct string" do
-      expect(solve.fizzbuzz(7)).to eq("7")
+    it 'should return the correct string' do
+      expect(solve.fizzbuzz(7)).to eq('7')
     end
   end
 end
